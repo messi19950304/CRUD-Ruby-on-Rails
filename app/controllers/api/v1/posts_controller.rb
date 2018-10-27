a767c008ed4866d95d8bf4a4dd8f8f6bd0499436
WDevStar <messi19950304@outlook.com>
2018-10-27 14:57:09 -0500

    crud posts

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,15 +1,15 @@
 module Api
     module V1
         class PostsController < ActionController::API
+            include ActionController::HttpAuthentication::Token::ControllerMethods
+
+            before_action :authenticate , only: [:create, :update, :destory]
             def index #get
                 @posts=Post.order('updated_at DESC')
                 if @posts
-                    render json: @posts
-                                            
-                 else
-                  
-                 render json: @posts.erros, status: :unprocessable_entity
-                                       
+                    render json: @posts                                            
+                 else                  
+                 render json: @posts.errors, status: :unprocessable_entity                                       
                     
                 end
                 
@@ -28,7 +28,13 @@ module Api
             end
 
             def create #post/param
-                
+                @post=@user.posts.new (post_params) 
+                if @post.save
+                    render json: @post, status: :created
+                else
+                    render json: @post.errors, status: :unprocessable_entity
+                end
+
             end
 
             def update #put|pacth/param
@@ -39,7 +45,15 @@ module Api
               
             end
             
-
+private 
+def post_params
+    params.require(:post).permit(:title,:body)
+end
+def authenticate
+authenticate_or_request_with_http_token do|token, options|
+@user=User.find_by(toke: token)
+end
+end
           
         end
     end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -17,7 +17,16 @@ module Api
         end
 
         def show #get/param
-            
+            @users=User.find(params[:id])
+            if @users
+                render json:@users
+                                         
+            else
+                    
+             render json:@users.erros, status: :unprocessable_entity
+                
+                
+            end
         end
 
         def create #post/param
@@ -33,11 +42,24 @@ module Api
         end
 
         def update #put|pacth/param
-            
+            @user= User.find(params[:id])
+             if @user.update  user_params
+
+                render json: @user, status: :update
+              else
+                render json: @user.errors, status: :unprocessable_entity
+               
+            end
         end
 
         def destroy #delete/param
-            
+            @user= User.find(params[:id])
+             if @user.destroy  
+                render json: @user,status: :delete
+              else
+                render json: @user.errors, status: :unprocessable_entity
+               
+            end  
         end
 private
 def user_params
